796333c185d86c21dca649fec12e5d025398121b
Alf Helge Jakobsen <alfhj@stud.ntnu.no>
2024-04-26 18:17:10 +0200

    Add cubic curve calculation

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from math import cos, sin, sqrt
 from pathlib import Path
 import numpy as np
 import json
@@ -93,3 +94,40 @@ center = transform.transform(CENTER_COORDS[0], CENTER_COORDS[1])
 
 def get_relative_coordinates(x, y):
     return (x - center[0], y - center[1])
+
+
+def calculate_cubic_curve(end: tuple[float], phi: float, length_subdivisions: int = 20):
+    """Calculates and returns a parametric cubic polynomial of a curve between
+    the points (0, 0) and (end[0], end[1]).
+    See https://publications.pages.asam.net/standards/ASAM_OpenDRIVE/ASAM_OpenDRIVE_Specification/latest/specification/09_geometries/09_06_param_poly3.html
+
+    Args:
+        end (tuple[float]): s and t coordinates of end-point
+        phi (float): angle of the curve at end point in radians, relative to the starting point.
+            The curve always starts with phi=0 at the start point s=t=p=0.
+            Example: phi=math.pi/2: the curve turns left 90 degree
+            Range: -pi/2 < phi < pi/2
+        length_subdivisions (int): how many parts the curve is split into to calculate the length
+    Returns:
+        params: tuple(float): parameters aU, aV, bU, bV, cU, cV, dU, dV.
+            pRange should be set to "normalized"
+        length: approximate length of the curve
+    """
+    sin_phi, cos_phi = sin(phi), cos(phi)
+    aU = aV = bV = 0
+    bU = end[0]
+    cU = -(cos_phi * (end[0] - end[1]) - end[0])
+    dU = (cos_phi * (end[0] - end[1]) - end[0])
+    cV = -(sin_phi * (end[0] - end[1]) - 3 * end[1])
+    dV = (sin_phi * (end[0] - end[1]) - 2 * end[1])
+
+    def u(p): return aU + bU * p + cU * p ** 2 + dU * p ** 3
+    def v(p): return aV + bV * p + cV * p ** 2 + dV * p ** 3
+    # u2 - u1 = 0 + bU * (p2 - p1) + cU * (p2^2 - p1^2) + dU * (p2^3 - p1^3)
+
+    ps = [i / length_subdivisions for i in range(length_subdivisions + 1)]
+    length = 0
+    for p1, p2 in zip(ps[:-1], ps[1:]):
+        length += sqrt((u(p2) - u(p1)) ** 2 + (v(p2) - v(p1)) ** 2)
+
+    return (aU, aV, bU, bV, cU, cV, dU, dV), length
